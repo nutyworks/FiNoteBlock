6a49938112e96636c1e8f7402b4024ff962b62d6
nutyworks <nutyworks@gmail.com>
2020-04-08 20:31:16 +0900

    add resource pack auto download

--- src/main/java/com/github/nutyworks/finoteblock/PlayerManager.kt
+++ src/main/java/com/github/nutyworks/finoteblock/PlayerManager.kt
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerResourcePackStatusEvent
 import java.util.*
 import kotlin.collections.HashMap
 
@@ -21,6 +22,15 @@ class PlayerManager : Listener {
         playerChannel[uuid] ?: moveChannel(uuid, "default", true)
         if (playerSetting[uuid] == null)
             FiNoteBlockPlugin.instance.playerManager.playerSetting[uuid] = UserSettings(uuid)
+
+        event.player.setResourcePack("https://github.com/HielkeMinecraft/OpenNoteBlockStudio/blob/master/datafiles/Data/extranotes.zip?raw=true",
+                "609d2ea415906ecba8171ade91bda1c1056244ab")
+    }
+
+    @EventHandler
+    private fun onResourcePackStats(event: PlayerResourcePackStatusEvent) {
+        if (event.status == PlayerResourcePackStatusEvent.Status.DECLINED) event.player.sendMessage("§cSome notes will not be played without the resource pack.")
+        else if (event.status == PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD) event.player.sendMessage("§cFailed to download the resource pack. Download the resource pack at §8 https://github.com/HielkeMinecraft/OpenNoteBlockStudio/blob/master/datafiles/Data/extranotes.zip?raw=true")
     }
 
     fun moveChannel(uuid: UUID, channel: String, force: Boolean = false) {
